0971d4577bf12a957226e1f1211afdc35ff075b5
MohamedZogh <ma.zoghlami@hotmail.fr>
2019-04-16 11:54:53 +0000

    Correction commit

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,60 +1,70 @@
-var map = new ol.Map({
-    target: 'map',
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM()
-      })
-    ],
-    view: new ol.View({
-      center: ol.proj.fromLonLat([3.974842,43.543136 ]),
-      zoom: 16
-      
-    })
-  });
-  
-  
-  
-  map.on('click', console.log);
-  //      ol.marker([3.974842,43.543136 ]).addTo(map);
-  
-  
-  document.getElementById('search').addEventListener('submit', function(evt){
-    evt.stopPropagation();
-    evt.preventDefault();
-    var datas = {};
-    var els = evt.target.querySelectorAll('[name]') ;
-    for(var i in els) {
-      if(els[i].name && els[i].value)
-        datas[els[i].name] = els[i].value ;
-    }
-    console.log(datas) ;
-  })
-  
-  
-  document.getElementById('ville').addEventListener('keypress', function(evt){
-    var q = evt.target.value.replace(' ', '+') ;
-    if(q.length >= 3) {
-      fetch('https://nominatim.openstreetmap.org/search?q='+q+'&format=json&countrycodes=FR&limit=500').then(function(response) {
-        return response.json();
-      })
-        .then(function(myJson) {
-          var container = document.getElementById('listVille') ;
-          container.innerHTML = '' ;
-          myJson.forEach(function(el) {
-            if(el.type == 'administrative') {
-              var L = document.createElement('li');
-              L.setAttribute('data-lat', el.lat);
-              L.setAttribute('data-lng', el.lon);
-              L.addEventListener('click', function(evt){
-                console.log('click', L)
-              });
-              L.innerText = el.display_name ;
-              container.appendChild(L) ;
-            }
-          })
-  //        console.log(JSON.stringify(myJson));
-        });
-      
-    }
-    
-  })
+function initMap() {
+  var myLatLng = {lat: -25.363, lng: 131.044};
+  var options = {
+    zoom : 8,
+    center : myLatLng,
+  };
+  // Create a map object and specify the DOM element
+  // for display.
+  var map = new google.maps.Map(document.getElementById('googleMap'), options);
+
+  var marker = new google.maps.Marker({
+    position:{lat: 46.25, lng:6},
+    map: map,
+    icon: 'https://www.flaticon.com/free-icon/weightlifting_9410'
+    });
+
+    var input = document.getElementById('ville');
+
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.bindTo('bounds', map);
+
+    // Specify just the place data fields that you need.
+    autocomplete.setFields(['place_id', 'geometry', 'name']);
+
+//    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // var infowindow = new google.maps.InfoWindow();
+    // var infowindowContent = document.getElementById('infowindow-content');
+    // infowindow.setContent(infowindowContent);
+
+    // var marker = new google.maps.Marker({map: map});
+
+    // marker.addListener('click', function() {
+    //   infowindow.open(map, marker);
+    // });
+
+    autocomplete.addListener('place_changed', function() {
+      // infowindow.close();
+
+      var place = autocomplete.getPlace();
+
+      if (!place.geometry) {
+        return;
+      }
+
+      if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+        console.log(place.geometry.viewport)
+      } else {
+        map.setCenter(place.geometry.location);
+        console.log(place.geometry.location)
+
+        map.setZoom(17);
+      }
+
+      // // Set the position of the marker using the place ID and location.
+      // marker.setPlace({
+      //   placeId: place.place_id,
+      //   location: place.geometry.location
+      // });
+
+      // marker.setVisible(true);
+
+      // infowindowContent.children['place-name'].textContent = place.name;
+      // infowindowContent.children['place-id'].textContent = place.place_id;
+      // infowindowContent.children['place-address'].textContent =
+      //     place.formatted_address;
+      // infowindow.open(map, marker);
+    });
+  }
